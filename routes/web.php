6d7e329cd35b93aeb773eb5fb1203a5a6e0aedbd
Mohamed Mostafa <mokammel0000@gmail.com>
2024-07-22 22:31:46 +0300

    extends master blade

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard/index', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
