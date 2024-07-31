e624e250ff0f02bc495419759a32dac002b8504f
Mohamed Mostafa <mokammel0000@gmail.com>
2024-07-31 03:43:40 +0300

    update dashboard views

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,68 +1,84 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Index')  {{-- Simple value --}}
+@push('styles')
+<link rel="stylesheet" href="{{asset('dist/css/adminlte2.min.css')}}">
+@endpush
+
+@push('styles')
+<link rel="stylesheet" href="{{asset('dist/css/adminlte3.min.css')}}">
+{{--will not override the previous one--}}
+@endpush
+
+
+@section('title', 'Index') {{-- Simple value --}}
 {{-- if you don't use html in the content, add it as a second parameter (to avoid writing endsection directive) --}}
 
+@section('breadcrumb')
+@parent {{-- to inheret parent section in addition to this section (if you don't write it, this will override parent's
+section--}}
+<li class="breadcrumb-item active">@yield('title')</li>
+@endsection
+
 @section('content')
-    
-    <!-- Main content -->
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body">
+
+<!-- Main content -->
+<div class="row">
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-body">
                 <h5 class="card-title">Card title</h5>
 
                 <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
+                    Some quick example text to build on the card title and make up the bulk of the card's
+                    content.
                 </p>
 
                 <a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>
-              </div>
             </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div><!-- /.card -->
-          </div>
-          <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <!-- /.col-md-6 -->
         </div>
-        <!-- /.row -->
-    <!-- /.content -->
+
+        <div class="card card-primary card-outline">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+
+                <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the card's
+                    content.
+                </p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div><!-- /.card -->
+    </div>
+    <!-- /.col-md-6 -->
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="m-0">Featured</h5>
+            </div>
+            <div class="card-body">
+                <h6 class="card-title">Special title treatment</h6>
+
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+
+        <div class="card card-primary card-outline">
+            <div class="card-header">
+                <h5 class="m-0">Featured</h5>
+            </div>
+            <div class="card-body">
+                <h6 class="card-title">Special title treatment</h6>
+
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <!-- /.col-md-6 -->
+</div>
+<!-- /.row -->
+<!-- /.content -->
 @endsection
