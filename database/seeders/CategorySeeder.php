3a9866b5d2953bdb75fbd7a76f2db96be3aad932
Mohamed Mostafa <mokammel0000@gmail.com>
2024-09-06 03:48:58 +0300

    edit category seeder to enable truncate method

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -58,7 +59,10 @@ class CategorySeeder extends Seeder
 
     public function run(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;'); // Disable foreign key checks
         Category::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;'); // Re-enable foreign key checks
+        // laravel cann't truncate the table because of it's a parent to another table, so we do this
 
         foreach ($this->categories as $category) {
             Category::create($category);
