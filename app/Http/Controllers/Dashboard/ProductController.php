b23aa388df929c6c3271b7ddcb623b5c243b859d
Mohamed Mostafa <mokammel0000@gmail.com>
2024-09-07 04:28:24 +0300

    add the product controller

---
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // local scope: you define it and explicitly using it when you need.
+        // gloabl scope: you define it, and it will be used automatically in all select queries.
+        $products = Product::paginate(5);
+        // $products = Product::active()->paginate(5);
+        return view('dashboard.products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
